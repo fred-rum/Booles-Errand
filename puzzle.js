87ca21d8a50241e74fad596887324dda42853486
fred-rum <fred-rum@users.noreply.github.com>
2016-05-23 18:36:40 -0700

    Another latch text update

--- puzzle.js
+++ puzzle.js
@@ -665,6 +665,7 @@ Level.prototype.puzzle = [
 ,
   {name: 'latch',
    intro: '<p>A <i>latch</i> allows an input data value to pass through to the output in certain conditions, but it holds its output constant by recirculating the last output value in other conditions.  The latch is described as <i>transparent</i> when data is allowed to pass through it and <i>opaque</i> when its output is held constant.  Whether a latch is transparent or opaque is typically determined by a clock signal connected to it.</p><p>The core recirculating elements of a latch are shown below. <b>Use appropriate gates to connect CLK (the clock signal) and&nbsp;A (the data input) such that A passes through to Z only when CLK is 1; otherwise Z is held constant.</b></p><p>Tip: the OR gate and AND gate recirculate the Z value only when their other inputs are certain values.  Otherwise, a new value replaces the recirculating value.</p>',
+   outro: '<p>Congratulations, <b>you have built a transparent-high SR latch.</b>  This means that the latch is transparent when the clock signal is <i>high</i>&nbsp;(1), and the recirculating value can be altered by <i>S</i>etting the value in the OR gate or <i>R</i>esetting the value in the AND gate.</p>',
    truth: [[{clk:0, a:0,   z:x},
             {clk:1, a:0,   z:0},
             {clk:1, a:1,   z:1},
