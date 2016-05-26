9a4828e06d160bb38a93f233bafd2207d920264d
fred-rum <fred-rum@users.noreply.github.com>
2016-05-26 09:15:27 -0700

    test

--- bdrag.js
+++ bdrag.js
@@ -3,6 +3,7 @@
 "use strict";
 
 function Bdrag() {
+  this.touchdata = {};
 }
 
 Bdrag.prototype.drag = function (jel, context, type, fn_start, fn_move, fn_end,
@@ -74,43 +75,61 @@ Bdrag.prototype.touchstart = function (data, event) {
   var t = e.changedTouches[0];
   //$('#info').append('<br>touchstart' + t.identifier);
   event.preventDefault();
-  if (this.dragging) return;
+  if (this.dragging == 'mouse') return;
+  if (this.touchdata[data.type]) return;
+
+  if (!this.dragging){
+    var doc = $(document);
+    doc.on("touchmove.booledrag", $.proxy(this.touchmove, this));
+    doc.on("touchend.booledrag", $.proxy(this.touchend, this));
+  }
   this.dragging = 'touch';
-  this.touchdata = data;
 
-  var doc = $(document);
-  doc.on("touchmove.booledrag", $.proxy(this.touchmove, this));
-  doc.on("touchend.booledrag", $.proxy(this.touchend, this));
-
-  this.mouseid = t.identifier;
+  data.mouseid = t.identifier;
+  this.touchdata[data.type] = data;
 
   data.fn_start.call(data.context, t.pageX, t.pageY, data.extra);
 };
 
 Bdrag.prototype.touchmove = function (event) {
   var e = event.originalEvent || event;
-  for (var i = 0; i < e.touches.length; i++) {
-    if (e.touches[i].identifier == this.mouseid){
-      this.touchdata.fn_move.call(this.touchdata.context,
-                                  e.touches[i].pageX, e.touches[i].pageY,
-                                  this.touchdata.extra);
+  var types = ['cdraw', 'speed', 'cell', 'io'];
+  for (var j = 0; j < types.length; j++) {
+    var type = types[j];
+    for (var i = 0; i < e.touches.length; i++) {
+      if (this.touchdata[type] &&
+          (e.touches[i].identifier == this.touchdata[type].mouseid)){
+        this.touchdata[type].fn_move.call(this.touchdata[type].context,
+                                          e.touches[i].pageX,
+                                          e.touches[i].pageY,
+                                          this.touchdata[type].extra);
+      }
     }
   }
 };
 
 Bdrag.prototype.touchend = function (event) {
   var e = event.originalEvent || event;
-  for (var i = 0; i < e.changedTouches.length; i++) {
-    //$('#info').append('<br>touchend' + e.changedTouches[i].identifier);
-    if (e.changedTouches[i].identifier == this.mouseid){
-      this.dragging = false;
-      var doc = $(document);
-      doc.off("touchmove.booledrag");
-      doc.off("touchend.booledrag");
-      this.touchdata.fn_end.call(this.touchdata.context,
-                                 e.changedTouches[i].pageX,
-                                 e.changedTouches[i].pageY,
-                                 this.touchdata.extra);
+  var types = ['cdraw', 'speed', 'cell', 'io'];
+  for (var j = 0; j < types.length; j++) {
+    var type = types[j];
+    for (var i = 0; i < e.changedTouches.length; i++) {
+      //$('#info').append('<br>touchend' + e.changedTouches[i].identifier);
+      if (this.touchdata[type] &&
+          (e.changedTouches[i].identifier == this.touchdata[type].mouseid)){
+        this.touchdata[type].fn_end.call(this.touchdata[type].context,
+                                         e.changedTouches[i].pageX,
+                                         e.changedTouches[i].pageY,
+                                         this.touchdata[type].extra);
+        this.touchdata[type] = undefined;
+      }
     }
+  }
+
+  if (!e.touches.length) {
+    this.dragging = false;
+    var doc = $(document);
+    doc.off("touchmove.booledrag");
+    doc.off("touchend.booledrag");
   }
 };
