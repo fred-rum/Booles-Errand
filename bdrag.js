cb39a5f13171a852326ae8ca19096d1f689f7aef
fred-rum <fred-rum@users.noreply.github.com>
2016-06-30 15:52:05 -0700

    Revert "test"
    This reverts commit db91b0f68549df938c02bf020abf4785ae39ae74.

--- bdrag.js
+++ bdrag.js
@@ -80,9 +80,9 @@ Bdrag.prototype.touchstart = function (data, event) {
 
   if (data.callbacks.start || data.callbacks.move || data.callbacks.end) {
     $('#info').html('');
-    $('#info').append('y start target');
+    $('#info').append('x start target');
     this.touch_append(e.targetTouches);
-    $('#info').append('y start changed');
+    $('#info').append('x start changed');
     this.touch_append(e.changedTouches);
 
     // It is possible for touchstart to be called with multiple touches
@@ -161,10 +161,6 @@ Bdrag.prototype.touch_append = function (touches) {
 // necessary since the iPad doesn't send proper changedTouches when
 // two touches end at once.)
 Bdrag.prototype.touchmoveetc = function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  event.stopImmediatePropagation();
-
   var e = event.originalEvent || event;
   // The touch types are processed in a certain order so that changes
   // to the canvas are made before cell dragging is updated.
